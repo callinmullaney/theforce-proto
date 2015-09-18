3b5e90da6448c0566dfbd7b92b3393aad11588c1
callinmullaney <callin@callinmullaney.me>
2015-09-18 10:21:39 -0500

    nav min and max size are set. also included transitional animations

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,4 +1,8 @@
 $('#theforce').click(function() {
   $('body.theforce').addClass('theforce-open');       
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJCgnI3RoZWZvcmNlJykuY2xpY2soZnVuY3Rpb24oKSB7XG4gICQoJ2JvZHkudGhlZm9yY2UnKS5hZGRDbGFzcygndGhlZm9yY2Utb3BlbicpOyAgICAgICBcbn0pOyJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
+
+$('#page').click(function() {
+  $('body.theforce').removeClass('theforce-open');       
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKCcjdGhlZm9yY2UnKS5jbGljayhmdW5jdGlvbigpIHtcbiAgJCgnYm9keS50aGVmb3JjZScpLmFkZENsYXNzKCd0aGVmb3JjZS1vcGVuJyk7ICAgICAgIFxufSk7XG5cbiQoJyNwYWdlJykuY2xpY2soZnVuY3Rpb24oKSB7XG4gICQoJ2JvZHkudGhlZm9yY2UnKS5yZW1vdmVDbGFzcygndGhlZm9yY2Utb3BlbicpOyAgICAgICBcbn0pOyJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,7 @@
 $('#theforce').click(function() {
   $('body.theforce').addClass('theforce-open');       
 });
+
+$('#page').click(function() {
+  $('body.theforce').removeClass('theforce-open');       
+});
